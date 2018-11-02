cd6034efe5a3022ebb213da092502ef837351d27
User <user@Users-MBP.hitronhub.home>
2018-11-02 02:09:14 -0700

    Separated view controllers

---
+++ MEAP - Game/MEAP - Game/HomeViewController.swift
@@ -0,0 +1,38 @@
+//
+//  HomeViewController.swift
+//  MEAP - Game
+//
+//  Created by User on 2018-11-02.
+//  Copyright © 2018 Angus Chen. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var CountdownLabel: UILabel!
+    @IBOutlet weak var StartingImage: UIImageView!
+    
+    @IBAction func StartButton(_ sender: Any) {
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MEAP - Game/MEAP - Game/PausedViewController.swift
@@ -0,0 +1,39 @@
+//
+//  PausedViewController.swift
+//  MEAP - Game
+//
+//  Created by User on 2018-11-02.
+//  Copyright © 2018 Angus Chen. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PausedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    var audioPlayer: AVAudioPlayer!;
+    @IBOutlet weak var ToggleSoundButton: UIButton!
+    
+    @IBAction func ToggleSound(sender: UIButton) {
+//        if (audioPlayer.isPlaying)
+//        {
+//            audioPlayer.pause();
+//            ToggleSoundButton.setImage(UIImage(named: "Turn Sound On"), for: UIControlState.normal)
+//        }
+//        else
+//        {
+//            audioPlayer.play();
+//         ToggleSoundButton.setImage(UIImage(named: "Turn Sound Off"), for: UIControlState.normal)
+        
+//        }
+    }
+    
+    
+}
